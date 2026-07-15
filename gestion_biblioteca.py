478b6e9d49485615edb77ca184403562fc85112e
martdmunoz-gif <martd.munoz@duocuc.cl>
2026-07-15 15:26:25 -0400

    Update gestion_biblioteca.py
    creamos el def de leer la opción que elija el usuario

--- gestion_biblioteca.py
+++ gestion_biblioteca.py
@@ -7,3 +7,12 @@ def mostrar_menu():
     print("5. Mostrar libros")
     print("6. Salir")
     print("=====================================")
+
+def leer_opcion():
+    while True:
+        opcion = input("Ingrese una opción (1-6): ").strip()
+        if opcion.isdigit():
+            opcion_num = int(opcion)
+            if 1 <= opcion_num <= 6:
+                return opcion_num
+        print("Opción no válida. Intente nuevamente.")
