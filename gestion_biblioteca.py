3c207342f75341e005a267cb5f65022fde71c74a
martdmunoz-gif <martd.munoz@duocuc.cl>
2026-07-15 15:24:38 -0400

    Update gestion_biblioteca.py
    creamos el menu principal

---
+++ gestion_biblioteca.py
@@ -0,0 +1,9 @@
+def mostrar_menu():
+    print("========== MENÚ PRINCIPAL ==========")
+    print("1. Agregar libro")
+    print("2. Buscar libro")
+    print("3. Eliminar libro")
+    print("4. Actualizar disponibilidad")
+    print("5. Mostrar libros")
+    print("6. Salir")
+    print("=====================================")
